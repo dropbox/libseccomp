25d881b29c490a3c9bf2e3c6ddcad828c73d5209
David Zbarsky <zbarsky@dropbox.com>
2016-08-05 21:57:51 +0000

    Check in the files we need to build the library/python bindings with Bazel Generated files with the following commands: ./autogen.sh ./configure

--- src/python/setup.py
+++ src/python/setup.py
@@ -25,11 +25,10 @@ import os
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
 setup(
 	name = "seccomp",
-	version = os.environ["VERSION_RELEASE"],
+	version = 1,
 	description = "Python binding for libseccomp",
 	long_description = "Python API for the Linux Kernel's syscall filtering capability, seccomp.",
 	url = "https://github.com/seccomp/libseccomp",
@@ -37,11 +36,10 @@ setup(
 	maintainer_email = "paul@paul-moore.com",
 	license = "LGPLv2.1",
 	platforms = "Linux",
-	cmdclass = {'build_ext': build_ext},
 	ext_modules = [
 		Extension("seccomp", ["seccomp.pyx"],
 			# unable to handle libtool libraries directly
-			extra_objects=["../.libs/libseccomp.a"],
+                        extra_objects=[os.environ["SECCOMP_PATH"]],
 			# fix build warnings, see PEP 3123
 			extra_compile_args=["-fno-strict-aliasing"])
 	]
